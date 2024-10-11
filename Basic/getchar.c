ca2278e2fee7014a077343be796369437a6aa658
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-10-11 10:35:53 +0530

    C_practices

---
+++ Basic/getchar.c
@@ -0,0 +1,22 @@
+// implement getchar()
+//function to read single character
+
+#include<stdio.h>
+int main()
+{
+ int character;
+ character=getchar();
+
+ printf("The entered character is : %c\n",character);
+
+ return 0;
+
+ }
+
+
+
+ /*
+ input :c
+ output:The entered character is : c
+
+ */

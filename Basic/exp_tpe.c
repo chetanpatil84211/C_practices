27f63fa1e3703cc2ca8f0f8fe9347c04453a655c
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-10-26 20:13:02 +0530

    C_practices

---
+++ Basic/exp_tpe.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+
+   double x=1.2;
+
+   //explicate convertion from double to int
+
+   int sum=(int)x+1;
+
+   printf("Sum is =%d\n",sum);
+
+
+
+
+return 0;
+}
+
+//Sum is =2
+

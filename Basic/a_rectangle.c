23b160c7221d4c51766738887efb8d4cd40f0044
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-10-30 19:34:36 +0530

    C_practices

---
+++ Basic/a_rectangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+  int length,width,result;
+
+  printf("Enter length : ");
+  scanf("%d",&length);
+  printf("Enter width : ");
+  scanf("%d",&width);
+
+  result=length*width;
+
+  printf("Area of Rectangular is : %d\n",result);
+
+
+  return 0;
+  }

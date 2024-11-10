d5353121b13a4a511ec0915ef778bfe67ef194b0
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-11-10 23:10:33 +0530

    C_practices

---
+++ Mcq/demo5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a n : ");
+    scanf("%d",&n);
+    
+    for(int i=1;i<=2*n-1;i++)
+     {  
+         if(i%2 !=0)
+         printf("%d\t",i);
+         
+     }
+     
+	 printf("\n");
+     return 0;
+}

30730ca062b4e8bb54d4d1f9d9ccf030dd703fcc
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-11-11 23:24:10 +0530

    C_practice

---
+++ function/hello.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void printhello();
+void printgoodbye();
+
+int main()
+{
+  printhello();
+  printgoodbye();
+
+  return 0;
+ }
+
+ void printhello()
+ {
+  printf("Hello\n");
+ }
+
+ void printgoodbye()
+ {
+   printf("Good Bye\n");
+ }
